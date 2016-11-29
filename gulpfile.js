313a27abae4468647ba4983057022392130b0703
jboschmans <joris.boschmans.u1757@student.hogent.be>
2016-11-29 09:14:08 +0100

    heroku

--- gulpfile.js
+++ gulpfile.js
@@ -92,3 +92,15 @@ gulp.task('default', ['html', 'browserify', 'styles'], function() {
   gulp.watch(viewFiles, ['views']);
   gulp.watch(jsFiles, ['browserify']);
 });
+
+gulp.task('heroku:production', ['html'], function() {
+  /*var html = gulp.src("build/index.html")
+                 .pipe(gulp.dest('./dist/'));
+
+  var js = gulp.src("build/main.js")
+               .pipe(uglify())
+               .pipe(gulp.dest('./dist/'));
+
+  return merge(html,js);*/
+  console.log("herko!!!!!!!!");
+});
